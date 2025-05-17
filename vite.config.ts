c08b74e9bb5146bbbbd87c296d8b307251616b7f
Consolation Mangena <consolationmangena@gmail.com>
2025-05-17 11:40:15 +0200

    Setup GitHub Pages deployment with custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+cimport { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/sitesurveyor/',
   server: {
     host: "::",
     port: 8080,
