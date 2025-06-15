552e61774537986dd8231549e45a4bc1b1244ae3
Consolation <111779527+consolationmangena@users.noreply.github.com>
2025-06-15 18:26:27 +0200

    Fix React Router basename for subdirectory deployment

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sitesurveyor">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/appstore" element={<AppStore />} />
